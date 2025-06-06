99e87f00fb616f9575b48e29b9169aa8a13b68c5
divyasaikrishnaavvaru <divyasaikrishna1996@gmail.com>
2025-06-07 00:35:17 +0530

    car life

--- CarManagement/Car.py
+++ CarManagement/Car.py
@@ -6,6 +6,12 @@ class car:
         self.makeyear = makeyear
         self.bodytype = bodytype
         self.transmission = transmission
+
+    def get_age(self):
+        return self.makeyear
+    
+    def resale_value(self, depricatedValue):
+        return self.price * (1 - depricatedValue)
     
     def __str__(self):
         return f"Car Model: {self.model} -  Car Make Year: {self.makeyear} - Price: {self.price} "
@@ -17,6 +23,15 @@ class ElectricCar(car):
         self.Supercharging = Supercharging
         self.Battery = Battery
         self.Warranty = Warranty
+
+    def battery_health(self):
+        return self.Battery * 0.4
+    
+    def charging_time(self):
+        return ( self.Battery * 60 )/ 10
+
+    def resale_value(self, depricatedValue, batteryLife):
+        return self.price * (1 - depricatedValue)
 
 class SportsCar(car):
     def __init__(self, price, milage, model, makeyear, bodytype, transmission, Horsepower, Torque, CoolingSystem, CO2Emissions, FuelCapacity, BrakeType):
